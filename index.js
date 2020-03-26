4101fe8cb8ce4e47c32ee7b70f0dbaef5248e525
Sgoldik <sgoldik@mail.ru>
2020-03-26 18:31:21 +0600

    0.0.2

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 const bitcoin = require('bitcoinjs-lib');
+const TESTNET = bitcoin.networks.testnet
 
-const keyPair1 = bitcoin.ECPair.makeRandom();
-const keyPair2 = bitcoin.ECPair.makeRandom();
-const keyPair3 = bitcoin.ECPair.makeRandom();
+const keyPair1 = bitcoin.ECPair.makeRandom({ network: TESTNET });
+const keyPair2 = bitcoin.ECPair.makeRandom({ network: TESTNET });
+const keyPair3 = bitcoin.ECPair.makeRandom({ network: TESTNET });
 
-let pubkeys = [];
-pubkeys.push(keyPair1.publicKey);
-pubkeys.push(keyPair2.publicKey);
-pubkeys.push(keyPair3.publicKey);
+let createMultiSig = (keyPair1, keyPair2, keyPair3) => {
+    let pubkeys = [];
+    pubkeys.push(keyPair1.publicKey);
+    pubkeys.push(keyPair2.publicKey);
+    pubkeys.push(keyPair3.publicKey);
 
-const redeem = bitcoin.payments.p2ms({m: 2, pubkeys});
-const p2sh = bitcoin.payments.p2sh({redeem: redeem});
+    const redeem = bitcoin.payments.p2ms(
+        {
+            m: 2,
+            pubkeys,
+            network: TESTNET
+        }
+    );
+    const p2sh = bitcoin.payments.p2sh(
+        {
+            redeem: redeem,
+            network: TESTNET
+        }
+    );
 
-console.log(p2sh.address);
-console.log('redeem script:', redeem.output.toString('hex'));
-showKey('  key 1:', keyPair1);
-showKey('  key 2:', keyPair2);
-showKey('  key 3:', keyPair3);
+    console.log(p2sh.address);
+    console.log('redeem script:', redeem.output.toString('hex'));
+    showKey('  key 1:', keyPair1);
+    showKey('  key 2:', keyPair2);
+    showKey('  key 3:', keyPair3);
 
-function showKey(txt, keyPair) {
-    console.log(txt, 'private key in WIF:', keyPair.toWIF(), 'public key in hex:', keyPair.publicKey.toString('hex'));
+    let showKey = (txt, keyPair) => {
+        console.log(txt, 'private key in WIF:', keyPair.toWIF(), 'public key in hex:', keyPair.publicKey.toString('hex'));
+    }
+
+}
+
+let sendTransaction = () => {
+
 }
