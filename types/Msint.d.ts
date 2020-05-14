5dde695dfd8bdc4ef29706ace9a85b43d2f3e3d5
Sgoldik <sgoldik@mail.ru>
2020-05-14 18:49:56 +0700

    0.1.1

--- types/Msint.d.ts
+++ types/Msint.d.ts
@@ -3,9 +3,9 @@ import { ECPair } from "bitcoinjs-lib";
 export declare class Msint {
     constructor(network: string);
 
-    getRedeemScript(pubKeys: Array): string
-    keyPairFromWIF(WIF: string): Array
-    keyPairsFromWIFs(WIFS: Array): Array
+    getRedeemScript(pubKeys: Array<string>): string
+    keyPairFromWIF(WIF: string): Array<string>
+    keyPairsFromWIFs(WIFS: Array<string>): Array<string>
     generateKeyPair(): ECPair
     generateKeyPairs(): Array<ECPair>
     bufferFromHex(hex: string): Buffer
